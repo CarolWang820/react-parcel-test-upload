ddda4c7c956710ee7d04aeea0ce906dbf8f6ea81
Carol Wang <xtwang0@uw.edu>
2021-04-16 07:26:08 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const App = () => {
+    return (
+        <svg style = {{border: "1px solid pink", width: 500, 
+        height: 500}}>
+            <circle cx="200" cy="200" r="5" />
+        </svg>
+    );
+};
+
+export default App;
+
