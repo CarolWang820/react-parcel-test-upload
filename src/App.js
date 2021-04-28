d2cc9bc8dd87122ced3b293f45602350d37d043a
Carol Wang <xtwang0@uw.edu>
2021-04-27 20:05:46 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import {csv} from "d3-fetch";
-import { scaleLinear } from
+import { useFetch } from "./hooks/useFetch";
+import { scaleLinear}  from "d3-scale";
+import { extent, max, min, bin } from "d3-array";
 
 const App = () => {
-    csv("https://github.com/CarolWang820/react-parcel-test-upload/blob/main/weather.csv")
-    .then((data) => console.log(data));
+    const [data, loading] = useFetch(
+        "https://github.com/CarolWang820/react-parcel-test-upload/blob/main/countries%20of%20the%20world.csv"
+      );
+    
+    const worldData = extent(data, (d) => {
+        return +d["Ladder score"];
+    });
     return (
         <div>
             <h1>Exploratory Data Analysis, Assignment2, INFO474</h1>
